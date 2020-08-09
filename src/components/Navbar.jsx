24dd118bc1be8b91ed26453e58a94da0e545b8bc
Shubham <ciruslivingstone@gmail.com>
2020-08-09 07:50:05 +0530

    Navbar Added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 const Navbar=()=>{
     return(
         <React.Fragment>
-            My Navbar Here
+            <nav class="navbar navbar-light bg-light">
+                <a class="navbar-brand" href="https://www.logic-square.com">
+                <img src="https://res.cloudinary.com/www-logic-square-com/image/upload/v1551945805/ls-logo.png" width="55" height="55" alt="" />
+                </a>
+                <strong className="badge badge-warning">DASHBOARD</strong>
+            </nav>
         </React.Fragment>
     );
 }
