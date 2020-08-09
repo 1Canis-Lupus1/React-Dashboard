cc48d3b6c82139f6604487e4c430a73052da01a1
Shubham <ciruslivingstone@gmail.com>
2020-08-09 08:03:21 +0530

    Adding new employees card added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 // import logo from './logo.svg';
 // import './App.css';
 import Navbar from './components/Navbar';
+import AddEmp from './components/Addnew';
 import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <AddEmp />
     </div>
   );
 }
---
+++ src/components/Addnew.jsx
@@ -0,0 +1,31 @@
+import React,{ Component } from 'react'
+
+class AddEmp extends Component{
+    constructor(){
+        super();
+
+        this.state={
+            avail: 0,
+            total: 0
+        }
+    }
+
+    render(){
+        return(
+            <React.Fragment>
+                <div class="card">
+                <div class="card-header">
+                    <span className='badge badge-success'>OverView</span>
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Available Employees: {this.state.avail}</h5>
+                    <h5 class="card-title">Total Employees: {this.state.total}</h5>
+                    <a href="#" class="btn btn-primary"><strong>+</strong> Add Employee</a>
+                </div>
+                </div>
+            </React.Fragment>
+        )
+    }
+}
+
+export default AddEmp;
